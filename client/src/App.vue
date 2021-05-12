27cf71e6e92ded8da19bd6206c1d99a63709d9ea
AriqHakim <ariq.hakim2002@gmail.com>
2021-05-12 21:19:54 +0700

    add navbar home button

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,26 +5,32 @@
         <div class="flex w-auto md:text-sm">
           <div class="font-sans">
             <router-link
+              to="/"
+              class="inline-block text-sm md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
+            >
+              Home
+            </router-link>
+            <router-link
               to="/novel"
-              class="inline-block text-base md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
+              class="inline-block text-sm md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
             >
               Novel
             </router-link>
             <router-link
               to="/author"
-              class="inline-block text-base md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
+              class="inline-block text-sm md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
             >
               Author
             </router-link>
             <router-link
               to="/illustrator"
-              class="inline-block text-base md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
+              class="inline-block text-sm md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
             >
               Illustrator
             </router-link>
             <router-link
               to="/label"
-              class="inline-block text-base md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
+              class="inline-block text-sm md:text-lg text-center px-4 py-2 leading-none rounded text-sepia-500 border-sepia-500 hover:border-transparent hover:text-steel-500 hover:bg-sepia-500"
             >
               Label
             </router-link>
@@ -39,17 +45,17 @@
 </template>
 
 <style>
-nav{
-    letter-spacing: 1px;
-    position: relative;
-    align-items: center;
-    width: 100%;
-    top: 100%;
-    left: 0;
+nav {
+  letter-spacing: 1px;
+  position: relative;
+  align-items: center;
+  width: 100%;
+  top: 100%;
+  left: 0;
 }
-header{
-    position: fixed;
-    z-index: 999;
+header {
+  position: fixed;
+  z-index: 999;
 }
 </style>
 
